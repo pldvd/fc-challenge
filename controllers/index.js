9554168479517fb7738e26de164617bd49405f30
David Pal <paldavid2030@gmail.com>
2020-02-18 11:25:29 +0300

    create show_all route handler

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
-'use strict'
+const Cache = require('../models/cache');
 
-exports.show_all = function (req, res) {
-  //define
-}
+exports.show_all = function (_, res) {
+  Cache.find()
+    .then(keys => {
+      return keys.length > 0 ? res.status(200).send(keys) : res.status(204).send('No keys are available')
+    })
+    .catch(err => {
+      res.status(400).send(`The following error occured: ${err.message}`)
+    });
 
 exports.delete_all = function (req, res) {
   //define
