1e0a1c7ac4da5d6f5cc92608a207d92d49bfc37c
David Pal <paldavid2030@gmail.com>
2020-02-18 10:34:33 +0300

    define routes and handlers

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+exports.show_all = function (req, res) {
+  //define
+}
+
+exports.delete_all = function (req, res) {
+  //define
+}
+
+exports.show_selected = function (req, res) {
+  //define 
+}
+
+exports.update_selected = function (req, res) {
+  //define
+}
+
+exports.delete_selected = function (req, res) {
+  //define
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controllers = require('../controllers/index');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/', controllers.show_all);
+router.delete('/', controllers.delete_all);
+router.get('/key/:id', controllers.show_selected);
+router.put('/key/:id', controllers.update_selected);
+router.delete('/key/:id', controllers.delete_selected);
 
 module.exports = router;
