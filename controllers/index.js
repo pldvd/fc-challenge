d5335dfe5dfea7824987d0f0344c6c0d40b086cc
David Pal <paldavid2030@gmail.com>
2020-02-18 13:51:02 +0300

    create show_selected route handler

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,15 @@
 const Cache = require('../models/cache');
 
+function randomString(length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 exports.show_all = function (_, res) {
   Cache.find()
     .then(keys => {
@@ -8,13 +18,33 @@ exports.show_all = function (_, res) {
     .catch(err => {
       res.status(400).send(`The following error occured: ${err.message}`)
     });
+}
 
 exports.delete_all = function (req, res) {
   //define
 }
 
 exports.show_selected = function (req, res) {
-  //define 
+  Cache.find({
+    key: req.params.key
+  })
+    .then(cacheEntry => {
+      if (cacheEntry.length === 0) {
+        console.log('Cache miss');
+        const newEntry = new Cache({
+          key: randomString(8),
+          data: randomString(50)
+        })
+
+        return newEntry.save()
+          .then(_ => res.status(201).send(newEntry.data))
+          .catch(err => res.status(400).send(`The following error occured: ${err.message}`))
+      }
+
+      console.log('Cache hit');
+      return res.status(200).send(cacheEntry);
+    })
+    .catch(err => res.status(400).send(`The following error occured: ${err.message}`));
 }
 
 exports.update_selected = function (req, res) {
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const controllers = require('../controllers/index');
 
 router.get('/', controllers.show_all);
 router.delete('/', controllers.delete_all);
-router.get('/key/:id', controllers.show_selected);
-router.put('/key/:id', controllers.update_selected);
-router.delete('/key/:id', controllers.delete_selected);
+router.get('/key/:key', controllers.show_selected);
+router.put('/key/:key', controllers.update_selected);
+router.delete('/key/:key', controllers.delete_selected);
 
 module.exports = router;
